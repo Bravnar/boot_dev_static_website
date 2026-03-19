e3eca50a0f3c115c345ae2de9123ed828ab1a2af
Bravnar <stan.muravyev@bluewin.ch>
2026-03-19 12:39:45 +0100

    commiting before testing dir deletion

--- src/main.py
+++ src/main.py
@@ -1,28 +1,35 @@
-from nodes.textnode import TextNode, TextType
-from utils.split_nodes_delimiter import split_nodes_delimiter
-from pprint import pprint
+import os
+import shutil
+
+
+def recursive_copy(to_copy, dest):
+    list_to_copy = os.listdir(to_copy)
+    for item in list_to_copy:
+        abs_path = os.path.join(to_copy, item)
+        if os.path.isfile(abs_path):
+            shutil.copy(abs_path, os.path.join(dest, item))
+        elif os.path.isdir(abs_path):
+            new_dir_path = os.path.join(dest, item)
+            os.mkdir(new_dir_path)
+            recursive_copy(abs_path, new_dir_path)
+
+
+def static_to_public(src="static/", dest="public/"):
+    print("Calling static to public function")
+    src_abs = os.path.abspath(src)
+    dest_abs = os.path.abspath(dest)
+    # print(src_abs, dest_abs, sep="\n")
+    if not (os.path.exists(src_abs) and os.path.isdir(src_abs)):
+        raise Exception("source directory does not exist or is not a directory")
+    if os.path.exists(dest_abs) and os.path.isdir(dest_abs):
+        shutil.rmtree(dest_abs)
+    os.mkdir(dest_abs)
+    recursive_copy(src_abs, dest_abs)
 
 
 def main():
-    # new_text_node = TextNode(
-    #     "This is some anchor text", TextType.LINK, "https://boot.dev"
-    # )
-    # print(new_text_node)
-    print("############# Testing split_nodes_delimiter")
-
-    nodes = [
-        TextNode("This phrase has **one** word that is bold", TextType.TEXT),
-        TextNode("This **one** here has **two** words that are bold", TextType.TEXT),
-        TextNode(
-            "This **one** here has **three** words that are **bold**", TextType.TEXT
-        ),
-        # TextNode("This **node should fail.", TextType.TEXT),
-        TextNode("This *node* should not count.", TextType.TEXT),
-    ]
-    try:
-        pprint(split_nodes_delimiter(nodes, "**", TextType.BOLD))
-    except Exception as e:
-        print(f"Whoops you hit an {e}")
+    print("Welcome to the Static Website Generator")
+    static_to_public()
 
 
 if __name__ == "__main__":
